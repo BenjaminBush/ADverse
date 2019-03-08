af5131b4d84623e128ee36f5c8c1eba394fe068c
Benjamin Bush <ben.bush4@gmail.com>
2019-03-07 18:06:06 -0600

    big update

--- attacks/resnet_evasion.py
+++ attacks/resnet_evasion.py
@@ -5,56 +5,109 @@ from keras.models import load_model
 from keras.applications.resnet50 import ResNet50
 import matplotlib.pyplot as plt
 from PIL import Image
+import pickle
+import os
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+
+config = tf.ConfigProto(
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
+    # device_count = {'GPU': 1}
+)
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+set_session(session)
+
+Image.MAX_IMAGE_PIXELS = 10000000000000000000      
+
 
 # instantiate model
 keras.backend.set_learning_phase(0)
-kmodel = load_model('../resnet.h5')
-print(kmodel.summary())
-# kmodel = ResNet50(weights='imagenet')
-# print(kmodel.summary())
-preprocessing = (np.array([104, 116, 123]), 1)
-fmodel = foolbox.models.KerasModel(kmodel, bounds=(0, 255), preprocessing=preprocessing)
+kmodel = load_model('../resnet_foolbox_test.h5')
+fmodel = foolbox.models.KerasModel(kmodel, bounds=(0, 255))
 
 # get source image and label
-img_path = "C:\\Users\\Ben\\Desktop\\new_data\\validation\\ad\\170090.png"
-image = Image.open(img_path)
-image = image.resize((224, 224))
-image = np.asarray(image, dtype=np.float32)
-image = image[:, :, :3]
-label = 0
+data_dir = "C:\\Users\\Ben\\Desktop\\data\\subfolder-9\\"
+num_evasions = 0
+num_files = 0
+processed = 0
+for i in range(0, 1):
+	directory = data_dir + str(i) + "\\"
+	f = open('../artifacts/preds_'+str(i), 'rb')
+	preds = pickle.load(f)
+	adversarial_images = {}
+	for filename in os.listdir(directory):
+		num_files += 1
+		fname = directory + filename
+		img = Image.open(fname)
+		img = img.resize((224, 224))
+		image = np.asarray(img, dtype=np.float32)
+		try:
+			image = image[:, :, :3]
+		except:
+			continue
+		label = 0
 
-# Get prediction for source image
-pred_label = np.argmax(fmodel.predictions(image[:, :, ::-1]))
-print(pred_label)
+		# Get prediction for source image
+		pred_label = np.argmax(fmodel.predictions(image[:, :, ::-1]))
+		print(pred_label)
 
-# apply attack on source image
-# ::-1 reverses the color channels, because Keras ResNet50 expects BGR instead of RGB
-attack = foolbox.attacks.FGSM(fmodel)
-adversarial = attack(image[:, :, ::-1], label)
-# if the attack fails, adversarial will be None and a warning will be printed
-# Get prediction for source image
-adv_pred_label = np.argmax(fmodel.predictions(adversarial[:, :, ::-1]))
+		# apply attack on source image
+		# ::-1 reverses the color channels, because Keras ResNet50 expects BGR instead of RGB
+		attack = foolbox.attacks.FGSM(fmodel)
+		adversarial = attack(image[:, :, ::-1], label)
+		processed += 1
+		if adversarial is None:
+			continue
+		else:
+			difference = adversarial[:, :, ::-1] - image
+			if np.sum(difference) == 0:
+				continue
+
+			num_evasions += 1
+			adversarial_images[fname] = adversarial
+
+			# # if the attack fails, adversarial will be None and a warning will be printed
+			# # Get prediction for source image
+			adv_pred_label = np.argmax(fmodel.predictions(adversarial[:, :, ::-1]))
 
 
 
-plt.figure()
+			# plt.figure()
 
-plt.subplot(1, 3, 1)
-plt.title('Original')
-plt.imshow(image / 255)  # division by 255 to convert [0, 255] to [0, 1]
-plt.axis('off')
-print(str(pred_label) + ", " + str(label))
+			# plt.subplot(1, 3, 1)
+			# plt.title('Original')
+			# plt.imshow(image / 255)  # division by 255 to convert [0, 255] to [0, 1]
+			# plt.axis('off')
+			# print(str(pred_label) + ", " + str(label))
 
-plt.subplot(1, 3, 2)
-plt.title('Adversarial')
-plt.imshow(adversarial[:, :, ::-1] / 255)  # ::-1 to convert BGR to RGB
-plt.axis('off')
-print(str(adv_pred_label) + ", " + str(label))
+			# plt.subplot(1, 3, 2)
+			# plt.title('Adversarial')
+			# plt.imshow(adversarial[:, :, ::-1] / 255)  # ::-1 to convert BGR to RGB
+			# plt.axis('off')
+			# print(str(adv_pred_label) + ", " + str(label))
 
-plt.subplot(1, 3, 3)
-plt.title('Difference')
-difference = adversarial[:, :, ::-1] - image
-plt.imshow(difference / abs(difference).max() * 0.2 + 0.5)
-plt.axis('off')
+			# plt.subplot(1, 3, 3)
+			# plt.title('Difference')
+			# difference = adversarial[:, :, ::-1] - image
+			# plt.imshow(difference / abs(difference).max() * 0.2 + 0.5)
+			# plt.axis('off')
 
-plt.show()
+			# plt.show()
+		if processed % 100 == 0:
+			print("Processed {}".format(processed))
+			print("Evaded {}".format(num_evasions))
+
+		# if processed > 500:
+		# 	processed = 0
+		# 	break
+
+
+	print("Saving...")
+	with open("../artifacts/adversarial_images_"+str(i), 'wb') as f:
+		pickle.dump(adversarial_images, f)
+	with open("../artifacts/stats_"+str(i), "w+") as f2:
+		f2.write("Number of files: {}\n".format(num_files))
+		f2.write("Number of evasions: {}\n".format(num_evasions))
+	print("Saved")
+	print(num_evasions)
--- train_model.py
+++ train_model.py
@@ -24,21 +24,22 @@ epochs = 100
 batch_size = 16
 
 # build the VGG16 network
-model = applications.ResNet50(weights=None, include_top=False, input_shape=(img_width, img_height, 3))
+model = applications.resnet50.ResNet50(weights=None)
 print('Model loaded.')
 
 # add custom layers
-x = model.output
-x = GlobalAveragePooling2D()(x)
-predictions = Dense(1, activation="sigmoid")(x)
+x = model.layers[-2].output
+predictions = Dense(2, activation="softmax", kernel_initializer="random_uniform")(x)
 
 
 model_final = Model(input=model.input, output=predictions)
 
 
-model_final.compile(loss='binary_crossentropy',
+model_final.compile(loss='categorical_crossentropy',
               optimizer=optimizers.SGD(lr=1e-3, momentum=0.9),
               metrics=['accuracy'])
+
+print(model_final.summary())
 
 # prepare data augmentation configuration
 train_datagen = ImageDataGenerator(
@@ -49,21 +50,34 @@ train_datagen = ImageDataGenerator(
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
+# train_generator = train_datagen.flow_from_directory(
+#     train_data_dir,
+#     target_size=(img_height, img_width),
+#     batch_size=batch_size,
+#     class_mode='binary')
+
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_height, img_width),
-    batch_size=batch_size,
-    class_mode='binary')
+    batch_size=batch_size)
+
+# validation_generator = test_datagen.flow_from_directory(
+#     validation_data_dir,
+#     target_size=(img_height, img_width),
+#     batch_size=batch_size,
+#     class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
     target_size=(img_height, img_width),
-    batch_size=batch_size,
-    class_mode='binary')
+    batch_size=batch_size)
 
 # Save the model according to the conditions  
-checkpoint = ModelCheckpoint("resnet.h5", monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+checkpoint = ModelCheckpoint("resnet.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
 early = EarlyStopping(monitor='val_acc', min_delta=0, patience=5, verbose=1, mode='auto')
+
+print("Train generator classes: {}".format(train_generator.class_indices))
+
 
 # fine-tune the model
 history = model_final.fit_generator(
